0fe9635b9a48d8dfb17f49a3c997c8bb1a1913f1
Andrea Cervesato <andrea.cervesato@suse.com>
2022-03-29 13:54:53 +0200

    Expose Suite and Test via ltp.metadata import

--- ltp/metadata/__init__.py
+++ ltp/metadata/__init__.py
@@ -5,11 +5,15 @@
 
 .. moduleauthor:: Andrea Cervesato <andrea.cervesato@suse.com>
 """
+from .base import Test
+from .base import Suite
 from .base import Metadata
 from .base import MetadataError
 from .runtest import RuntestMetadata
 
 __all__ = [
+    "Test",
+    "Suite",
     "Metadata",
     "MetadataError",
     "RuntestMetadata",
