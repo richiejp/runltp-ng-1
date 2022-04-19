5ce02d5d262d509920068828641ea0ef15019c57
Richard Palethorpe <rpalethorpe@suse.com>
2022-04-19 11:20:11 +0100

    Override base expect and add version msg tests

--- ltx/test.py
+++ ltx/test.py
@@ -1,11 +1,44 @@
 import pytest
 import signal
+import sys
+import subprocess as sp
+import threading
+from queue import Queue, Empty
+
 from msgpack import packb, unpackb
 from pexpect import run
-from pexpect.popen_spawn import PopenSpawn as spawn
+from pexpect.popen_spawn import PopenSpawn
+from pexpect.spawnbase import SpawnBase
+
+class LtxSpawn(PopenSpawn):
+    """Overrides PopenSpawns __init__ to seperate stderr from stdout
+
+    Copy and pasted from popen_spawn.py
+    """
+    def __init__(self):
+        super(PopenSpawn, self).__init__(timeout=1)
+
+        self.proc = sp.Popen(
+            ["./ltx"],
+            bufsize=0,
+            stdin=sp.PIPE,
+            stderr=sp.PIPE,
+            stdout=sp.PIPE,
+        )
+        self.pid = self.proc.pid
+        self.closed = False
+        self._buf = self.string_type()
+
+        self._read_queue = Queue()
+        self._read_thread = threading.Thread(target=self._read_incoming)
+        self._read_thread.setDaemon(True)
+        self._read_thread.start()
 
 CFLAGS = '-Wall -Wextra -Werror -fno-omit-frame-pointer -fsanitize=address,undefined'
 CFILES = 'ltx.c -o ltx'
+
+def spawn():
+    return LtxSpawn()
 
 class TestLtx:
     def test_compile_gcc(self):
@@ -18,24 +51,41 @@ class TestLtx:
         print(log)
         assert status == 0
 
+    def test_version_nolib(self):
+        p = spawn()
+
+        assert p.expect_exact(b'\x93\x02\xc0\xd9/[ltx.c:main:245] Linux Test Executor 0.0.1-dev\n') == 0
+
+        p.kill(signal.SIGTERM)
+        p.wait()
+
+    def test_version(self):
+        p = spawn()
+        p.logfile = sys.stdout.buffer
+
+        assert p.expect_exact(packb([2, None, "[ltx.c:main:245] Linux Test Executor 0.0.1-dev\n"])) == 0
+
+        p.kill(signal.SIGTERM)
+        p.wait()
+
     def test_ping_nolib(self):
-        p = spawn('./ltx', timeout=1)
+        p = spawn()
 
         # Ping: [0]
         assert p.send(b'\x91\x00') == 2
-        # Pong: [1]
-        assert p.expect(b'\x91\x01') == 0
+        # Pong: [0]
+        assert p.expect_exact(b'\x91\x00') == 0
 
         p.kill(signal.SIGTERM)
         p.wait()
 
     def test_ping(self):
-        p = spawn('./ltx', timeout=1)
+        p = spawn()
 
         # Ping
         assert p.send(packb([0])) == 2
         # Pong
-        assert p.expect(packb([1])) == 0
+        assert p.expect_exact(packb([0])) == 0
 
         p.kill(signal.SIGTERM)
         p.wait()
