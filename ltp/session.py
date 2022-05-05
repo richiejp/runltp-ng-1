dbf7f87244346ca7963c521ace5a98bc438c45bb
Andrea Cervesato <andrea.cervesato@suse.com>
2022-05-05 14:38:36 +0200

    Increase debug.log information

--- ltp/session.py
+++ ltp/session.py
@@ -133,7 +133,7 @@ class Session:
         handler.setLevel(logging.DEBUG)
 
         formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+            "%(asctime)s - %(name)s:%(lineno)s - %(levelname)s - %(message)s")
         handler.setFormatter(formatter)
         logger.addHandler(handler)
 
